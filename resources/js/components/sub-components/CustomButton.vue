74b318b824b30cc6c7446feeac1c9ac7234b01ca
tatarnirartha <tatarnirartha@gmail.com>
2022-01-27 18:47:47 +0800

    Creating CustomButton component and adding fortawesome

---
+++ resources/js/components/sub-components/CustomButton.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <button type="button" class="btn" :class="btn_class" @click="handleClick" :style="styles">
+            <i v-if="icon_class != ''" :class="icon_class"></i>
+            <span>
+                {{label}}
+            </span>
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CustomButton',
+    props: {
+        icon_class: {
+            type: String,
+            default: ''
+        },
+        btn_class: {
+            type: String,
+            default: 'btn-primary'
+        },
+        styles: {
+            type: String,
+            default: ''
+        },
+        label: {
+            type: String,
+            default: ''
+        }
+    },
+    methods: {
+        handleClick(){
+            this.$emit('btnClick');
+        } 
+    }
+}
+</script>
