5ef70b5742c62a58634c396cd82d89434a00948c
tatarnirartha <tatarnirartha@gmail.com>
2022-01-28 15:16:50 +0800

    create InstructionTable component, BreadCrumb component, PageTitleComponent, and InstructionCard component(not done yet)

---
+++ resources/js/components/sub-components/BreadCrumb.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <nav style="--bs-breadcrumb-divider: '>'" aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item" v-for="(data, index) in datas" :key="index">
+                <router-link class="text-muted" :to="{name: data.to}">{{data.name}}</router-link>
+            </li>
+        </ol>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "BreadCrumb",
+    props: {
+        datas: {
+            type: Array,
+            default: () => {
+                return []
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+a {
+    text-decoration: none;
+}
+</style>
