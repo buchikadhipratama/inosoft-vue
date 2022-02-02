8d50723301fbc6ebf0df9011065371a220345963
manuaba42 <ssedanayoga3@gmail.com>
2022-02-03 00:06:15 +0800

    create item controller & items table

--- database/migrations/2022_02_02_144421_create_items_table.php
+++ database/migrations/2022_02_02_144421_create_items_table.php
@@ -17,13 +17,13 @@ class CreateItemsTable extends Migration
             $table->enum('vendor', ['Amarit & Associates Co Ltd', 'Amarit & Associates Logistics Co Ltd', 'Alphatrans Pte Ltd', 'Bintang Mas Shipping Pte Ltd', 'Bangkok Freight Forwarders Co Ltd']);
             $table->string('attention');
             $table->string('quotation');
-            $table->enum('invoice');
-            $table->enum('customer_contract');
-            $table->enum('vendor_address');
+            $table->enum('invoice', ['Marubeni-Ithochu Tubulars', 'Marubeni-Ithochu Steel', 'MITME', 'MITE']);
+            $table->enum('customer_contract', ['Hail & Gasha', 'OFFSHORE', 'ONSHORE', 'Sour Gas']);
+            $table->enum('vendor_address', ['Soi Pridhi']);
             $table->string('customer_po');
             $table->string('description');
             $table->integer('qty');
-            $table->enum('uom');
+            $table->enum('uom', ['SHP']);
             $table->integer('unit_price');
             $table->integer('discount');
             $table->integer('gst_vat');
@@ -31,7 +31,7 @@ class CreateItemsTable extends Migration
             $table->enum('change', ['MITME', 'Customer']);
             $table->string('attachment');
             $table->text('notes');
-            $table->enum('link_to');
+            $table->enum('link_to', ['INSP-2020-0001', 'INSP-2020-0002', 'INSP-2020-0003', 'INSP-2020-0004']);
         });
     }
 
