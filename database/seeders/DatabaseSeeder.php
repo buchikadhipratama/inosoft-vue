ce96acd1f78b3c693153f8ea5345302f0c34183c
tatarnirartha <tatarnirartha@gmail.com>
2022-02-16 23:03:44 +0800

    Fix DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,24 +28,27 @@ class DatabaseSeeder extends Seeder
         DB::collection('statusses')->delete();
         DB::collection('recipients')->delete();
 
-        DB::collection('assign_vendors')->insert(
+        DB::collection('assign_vendors')->insert([
             ['assign_vendor' => 'Amarit & Associates Co Ltd'],
             ['assign_vendor' => 'Amarit & Associates Logistics Co Ltd'],
             ['assign_vendor' =>'Alphatrans Pte Ltd'], 
-            ['assign_vendor' =>'Bangkok Freight Forwarders Co Ltd']);
+            ['assign_vendor' =>'Bangkok Freight Forwarders Co Ltd']
+        ]);
 
 
-        DB::collection('invoices')->insert(
+        DB::collection('invoices')->insert([
             ['invoice' => 'Marubeni-Ithochu Tubulars'],
             ['invoice' => 'Marubeni-Ithochu Steel'], 
             ['invoice' => 'MITME'], 
-            ['invoice' => 'MITE']);
+            ['invoice' => 'MITE']
+        ]);
 
-        DB::collection('customer_contracts')->insert(
+        DB::collection('customer_contracts')->insert([
             ['customer_contract' => 'Hail & Gasha'],
             ['customer_contract' => 'OFFSHORE'], 
             ['customer_contract' => 'ONSHORE'], 
-            ['customer_contract' => 'Sour Gas']);
+            ['customer_contract' => 'Sour Gas']
+        ]);
 
         DB::collection('vendor_addresses')->insert(
             ['vendor_address' => 'Soi Pridhi']);
@@ -53,24 +56,28 @@ class DatabaseSeeder extends Seeder
         DB::collection('uoms')->insert(
             ['uom' => 'SHP']);
 
-        DB::collection('currencys')->insert(
+        DB::collection('currencys')->insert([
             ['currency' => 'USD'],
-            ['currency' => 'AED']);
+            ['currency' => 'AED']
+        ]);
 
-        DB::collection('changes')->insert(
+        DB::collection('changes')->insert([
             ['change' => 'MITME'],
-            ['change' => 'Customer']);
+            ['change' => 'Customer']
+        ]);
 
-        DB::collection('link_tos')->insert(
+        DB::collection('link_tos')->insert([
             ['link_to' => 'INSP-2020-0001'],
             ['link_to' => 'INSP-2020-0002'],
             ['link_to' =>  'INSP-2020-0003'], 
-            ['link_to' =>  'INSP-2020-0004']);
+            ['link_to' =>  'INSP-2020-0004']
+        ]);
 
-        DB::collection('statusses')->insert(
+        DB::collection('statusses')->insert([
             ['status_id' => '1', 'status' => 'in progress'],
             ['status_id' => '2', 'status' => 'completed'],
-            ['status_id' => '0', 'status' =>  'canceled']);
+            ['status_id' => '0', 'status' =>  'canceled']
+        ]);
 
         DB::collection('recipients')->insert(
             ['recipient' => 'thiar@inosoft.com']);
--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
 use App\Http\Controllers\ItemController;
-=======
-use App\Http\Controllers\VendorController;
->>>>>>> mongodb
 
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
-<<<<<<< HEAD
-// Route::any('/{any}', function () {
-//     return view('welcome');
-// });
-
-Route::get('/', [ItemController::class, 'index']);
-Route::get('/add', [ItemController::class, 'create']);
-Route::post('/add', [ItemController::class, 'store']);
-Route::get('/edit/{id}', [ItemController::class, 'edit']);
-Route::post('/edit/{id}', [ItemController::class, 'update']);
-Route::delete('/{id}', [ItemController::class, 'destroy']);
-=======
 Route::get('/{any}', function () {
     return view('welcome');
 });
->>>>>>> mongodb
